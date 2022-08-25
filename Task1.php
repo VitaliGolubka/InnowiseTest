5cd97226be4aaa9182aa4da6a6c3405ef9f08f98
danchikov_a <danchikovartem@gmail.com>
2022-08-25 12:07:08 +0300

    Created function to test whether a number is greater than 30, 20 or 10 using a ternary operator.

---
+++ Task1.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace src;
+
+class Task1
+{
+    const MORE_THAN_30_TEXT = "More than 30";
+    const MORE_THAN_20_TEXT = "More than 20";
+    const MORE_THAN_10_TEXT = "More than 10";
+    const EQUAL_OR_LESS_THAT_10_TEXT = "Equal or less than 10";
+
+    function checkNumber(int $number): string
+    {
+        $result = $number > 30
+            ? self::MORE_THAN_30_TEXT
+            : ($number > 20
+                ? self::MORE_THAN_20_TEXT
+                : ($number > 10
+                    ? self::MORE_THAN_10_TEXT
+                    : self::EQUAL_OR_LESS_THAT_10_TEXT));
+
+        return $result . "\n";
+    }
+}
